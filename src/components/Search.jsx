62b999a41dcabc8da042b241a671ee9425f74f82
Ryan Gregory <ryan.appleid@hotmail.co.uk>
2020-09-09 15:48:26 +0100

    added search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Search() {
   const [showSearch, setShowSearch] = useState(false);
+
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowSearch(false);
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  const searchRef = useRef();
+  useOutsideAlerter(searchRef);
+
+  function handleSearchClick() {
+    !showSearch ? setShowSearch(true) : performSearch();
+  }
+
+  function performSearch() {
+    alert("searching");
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") performSearch();
+  }
+
   return (
-    <>
-      <i className="icon dls-icon-search icon-hover" onClick={() => setShowSearch(true)} />
-      <input type="text" className={showSearch ? "" : ""} />
-      {showSearch && (
-        <i
-          className="icon dls-icon-cancel-circle icon-hover"
-          onClick={() => setShowSearch(false)}
-        />
-      )}
-    </>
+    <div className="search" ref={searchRef}>
+      <input
+        type="text"
+        id="search-input"
+        className={showSearch ? "animate" : ""}
+        placeholder="Find Someone"
+        onKeyDown={handleKeyDown}
+      />
+      <i className="icon dls-icon-search icon-hover search-icon" onClick={handleSearchClick} />
+    </div>
   );
 }
